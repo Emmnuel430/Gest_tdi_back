670bab95b0d1935da09f849c05013d72d8816032
joel.daho <joel.daho@asnumeric.com>
2025-10-01 04:35:00 +0000

    8th Commit-3.1

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -49,9 +49,7 @@ class Kernel extends HttpKernel
 
     /**
      * The application's route middleware.
-     *
      * These middleware may be assigned to groups or used individually.
-     *
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
